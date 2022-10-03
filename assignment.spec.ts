074fbefe16cfe90b1973a122a06427b53ede2df7
Sara Mohammadisedaghat <sara.mohammadisedaghat@folksam.se>
2022-10-03 16:58:22 +0200

    Add tester for assignment

--- assignment.spec.ts
+++ assignment.spec.ts
@@ -1,11 +1,38 @@
-import { expectedResult, getAllAssignments, getUserById } from "./assignmentClient";
+import {
+  expectedResult,
+  getAllAssignments,
+  getUserById,
+} from "./assignmentClient";
 
 describe("display a list of all the assignment with username and number of assignments per user", () => {
-    it("should be sorted on by username and assignment id", async () => {
-        const result = await getAllAssignments(); 
-        
+  it("should be sorted on by username and assignment id", async () => {
+    const result = await getAllAssignments();
 
-        // Assert
-        expect(result).toEqual(expectedResult);
-    });
-})
+    type userWithnumberOfPostsType = {
+      id: string;
+      name: string;
+      numberOfPosts: number;
+    };
+    const resultWithUser = await Promise.all(
+      result.map(async (assignment: any) => {
+        const userObj = (await getUserById(
+          assignment.userId
+        )) as userWithnumberOfPostsType;
+        userObj.numberOfPosts = result.filter(
+          (assignment: any) => assignment.userId == userObj.id
+        ).length;
+        const { userId, ...resultWithOutUserId } = assignment;
+
+        return { ...resultWithOutUserId, user: userObj };
+      })
+    );
+
+    resultWithUser.sort(
+      (a, b) => a.user.name.localeCompare(b.user.name) || a.id - b.id
+    );
+    console.log(resultWithUser);
+
+    // Assert
+    expect(resultWithUser).toEqual(expectedResult);
+  });
+});
