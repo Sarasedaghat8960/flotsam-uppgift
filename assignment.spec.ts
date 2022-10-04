11b2392c65c210f6e68bc36e31d248165b96fee9
Sara Mohammadisedaghat <sara.mohammadisedaghat@folksam.se>
2022-10-04 10:04:30 +0200

    add some comments

--- assignment.spec.ts
+++ assignment.spec.ts
@@ -15,22 +15,25 @@ describe("display a list of all the assignment with username and number of assig
     };
     const resultWithUser = await Promise.all(
       result.map(async (assignment: any) => {
-        const userObj = (await getUserById(
+        //Find user with same assignment's userId
+        const userWithnumberOfPostsType = (await getUserById(
           assignment.userId
         )) as userWithnumberOfPostsType;
-        userObj.numberOfPosts = result.filter(
-          (assignment: any) => assignment.userId == userObj.id
+        //Add numberOfPosts to user object
+        userWithnumberOfPostsType.numberOfPosts = result.filter(
+          (assignment: any) => assignment.userId == userWithnumberOfPostsType.id
         ).length;
+        // devide or delete userId from result
         const { userId, ...resultWithOutUserId } = assignment;
-
-        return { ...resultWithOutUserId, user: userObj };
+        // add and return result with user
+        return { ...resultWithOutUserId, user: userWithnumberOfPostsType };
       })
     );
 
     resultWithUser.sort(
       (a, b) => a.user.name.localeCompare(b.user.name) || a.id - b.id
     );
-    console.log(resultWithUser);
+    // console.log(resultWithUser);
 
     // Assert
     expect(resultWithUser).toEqual(expectedResult);
